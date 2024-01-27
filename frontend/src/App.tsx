e7a805c3a5d4921d03d8c576d46f3e3aa9316121
Devesh Krishan <deveshkrishan2003@gmail.com>
2024-01-26 22:15:11 -0800

    home page done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+
 import './App.css'
+import Home from './Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        
+        <Home/>
       </div>
     </>
   )
