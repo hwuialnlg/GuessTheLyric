86e5ff6fed5189278e7abc4b7cfd1b23742ba246
Andy Nguyen <iam.andynguwin@gmail.com>
2024-01-26 22:55:01 -0800

    Added a function to search by multiple artists

--- backend/spotipy_searches.py
+++ backend/spotipy_searches.py
@@ -1,6 +1,4 @@
-# import json
 from secret import *
-# import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
@@ -8,12 +6,42 @@ client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, clien
 SP = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 def search_by_artist(artist: str, limit: int) -> [str]:
+    '''
+    Retrieve a list of song names made by the inputted artist.
+
+    Args:
+        artist: The name of the artist to search songs for
+        limit: the maximum amount of songs to search for from the artist
+    
+    Returns:
+        A list of songs made by the inputted artist.
+    '''
+    artist = artist.lower()
     songs = []
     results = SP.search(artist, limit, type='track')
     for i in results['tracks']['items']:
-        if i['artists'][0]['name'] == artist:
+        if i['artists'][0]['name'].lower() == artist:
             songs.append(i['name'])
     return songs
 
+def search_by_x_artists(artists: (str), limit: int) -> [str]:
+    '''
+    Retrieve a list of song names made by any of the inputted artists.
+
+    Args:
+        artists: A tuple of artist names to search songs for
+        limit: the maximum amount of songs to search for from each artist
+    
+    Returns:
+        A list of songs made by any of the inputted artists.
+    '''
+    all_songs = []
+    for artist in artists:
+        songs = search_by_artist(artist, limit)
+        all_songs.extend(songs)
+    return all_songs
+
+
 if __name__ == "__main__":
     print(search_by_artist('keshi', 30))
+    print(search_by_x_artists(('keshi', 'dpr ian'), 30))
