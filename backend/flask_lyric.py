eff4cf4d98b23e42dd11713d2e42342c7a001aa5
Will Huang <96509348+hwuialnlg@users.noreply.github.com>
2024-01-28 02:38:49 -0800

    Added images to be sent to the frontend
    Added another Flask post route for images to be sent to the frontend

--- backend/flask_lyric.py
+++ backend/flask_lyric.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/lyrics", methods=["POST"])
-def send_data():
+def send_lyric_data():
     '''
     Flask connection sending backend lyric, artist, and song data to frontend
     '''
@@ -22,6 +22,8 @@ def send_data():
         artist = receive[0][0]
         songs = search_by_artist(artist)
 
+        # img = get_artist_pic(artist)
+
         for song, artist in songs:
             big_data.append(get_game_lyrics(artist, get_lyrics(artist, song), song))
 
@@ -33,6 +35,30 @@ def send_data():
     
 
     return jsonify({'response': big_data})
+
+
+@app.route("/image", methods=["POST"])
+def send_image():
+    '''
+    Flask connection sending backend of image to frontend
+    '''
+    receive = request.json['message'] # will receive artist(s)
+    
+    print(receive)
+
+    dct = {}
+
+    if len(receive[0]) == 1:
+        artist = receive[0][0]
+
+        dct = get_artist_pic(artist)
+
+    else:
+
+       dct = get_artist_pics({artist for song, artist in receive[0]})
+
+    return jsonify({'response': dct})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
